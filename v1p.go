34ded64fe3f3ca548a368ccdcc0303b52f01177a
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-07-25 11:02:50 -0300

    L+E+T

--- v1p.go
+++ v1p.go
@@ -12,20 +12,19 @@ import (
 )
 
 const (
-	L = "[v1p] "
+	T = "[v1p] "
 )
 
 var (
-	F    = fmt.Sprintf
-	logi func(string) error
-	loge func(string) error
+	L func(string, ...interface{})
+	E func(string, ...interface{})
 )
 
 func forward(local net.Conn, remoteAddr *string, timeout int) {
 	dialer := net.Dialer{Timeout: time.Second * time.Duration(timeout)}
 	remote, err := dialer.Dial("tcp", *remoteAddr)
 	if remote == nil {
-		loge(F("remote dial failed: %v", err))
+		E("remote dial failed: %v", err)
 		return
 	}
 	go io.Copy(local, remote)
@@ -33,16 +32,16 @@ func forward(local net.Conn, remoteAddr *string, timeout int) {
 }
 
 func vip(l, r *string, t int) {
-	logi(F("proxying %s to %s (t:%d)...", *l, *r, t))
+	L("proxying %s to %s (t:%d)...", *l, *r, t)
 	local, err := net.Listen("tcp", *l)
 	if local == nil {
-		loge(F("cannot listen: %v", err))
+		E("cannot listen: %v", err)
 		os.Exit(1)
 	}
 	for {
 		conn, err := local.Accept()
 		if conn == nil {
-			loge(F("accept failed: %v", err))
+			E("accept failed: %v", err)
 			os.Exit(1)
 		}
 		go forward(conn, r, t)
@@ -69,18 +68,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	log.SetPrefix(L)
+	log.SetPrefix(T)
 
 	if *s {
-		syslog, err := syslog.New(syslog.LOG_INFO, L)
+		syslog, err := syslog.New(syslog.LOG_INFO, T)
 		if err != nil {
-			log.Fatal("v1p: ", err)
+			log.Fatal(err)
 		}
-		logi = syslog.Info
-		loge = syslog.Err
+		L = func(f string, a ...interface{}) { syslog.Info(fmt.Sprintf(f, a...)) }
+		E = func(f string, a ...interface{}) { syslog.Err(fmt.Sprintf(f, a...)) }
 	} else {
-		logi = func(s string) (err error) { log.Println(s); return }
-		loge = func(s string) (err error) { log.Println(s); return }
+		L = func(f string, a ...interface{}) { log.Println(fmt.Sprintf(f, a...)) }
+		E = L
 	}
 
 	if *l != "" && *r != "" {
